8e025fb85ebb9e6b22b41315acc1d9ebba239312
Khalidprithy <khalidprithy@gmail.com>
2023-04-11 02:41:25 +0600

    ci: middle updated

--- pages/middleware.js
+++ pages/middleware.js
@@ -1,29 +1,29 @@
-import Cookies from 'js-cookie';
-import { verify } from 'jsonwebtoken';
-import { NextResponse } from 'next/server';
+// import Cookies from 'js-cookie';
+// import { verify } from 'jsonwebtoken';
+// import { NextResponse } from 'next/server';
 
-const secret = process.env.SECRET_TOKEN;
+// const secret = process.env.SECRET_TOKEN;
 
-export default function middleware(req) {
-  if (typeof window === 'undefined') {
-    return NextResponse.next();
-  }
+// export default function middleware(req) {
+//   if (typeof window === 'undefined') {
+//     return NextResponse.next();
+//   }
 
-  const userToken = Cookies.get('userToken');
-  const url = req.url;
+//   const userToken = Cookies.get('userToken');
+//   const url = req.url;
 
-  if (url.includes('/admin')) {
-    if (!userToken) {
-      return NextResponse.redirect('/login');
-    }
+//   if (url.includes('/admin')) {
+//     if (!userToken) {
+//       return NextResponse.redirect('/login');
+//     }
 
-    try {
-      verify(userToken, secret);
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect('/login');
-    }
-  }
+//     try {
+//       verify(userToken, secret);
+//       return NextResponse.next();
+//     } catch (error) {
+//       return NextResponse.redirect('/login');
+//     }
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
