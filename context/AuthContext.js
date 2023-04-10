e928e30b34007a9fa10e96d77e3747d82fb88b65
Khalidprithy <khalidprithy@gmail.com>
2023-04-11 00:36:06 +0600

    ci: middle updated

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,29 +1,119 @@
 import Cookies from 'js-cookie';
-import { verify } from 'jsonwebtoken';
-import { NextResponse } from 'next/server';
+import { useRouter } from 'next/router';
+import { createContext, useState } from 'react';
+import { toast } from 'react-hot-toast';
 
-const secret = process.env.SECRET_TOKEN;
+// Step 1: Create a context object
+const MyContext = createContext();
 
-export default function middleware(req) {
-  if (typeof window === 'undefined') {
-    return NextResponse.next();
-  }
+function MyContextProvider(props) {
+  const router = useRouter();
+  const [user, setUser] = useState(() =>
+    Cookies.get('shopInUser')
+      ? JSON.parse(Cookies.get('shopInUser'))
+      : null
+  );
 
-  const userToken = Cookies.get('userToken');
-  const url = req.url;
+  const [tempUser, setTempUser] = useState(() =>
+    Cookies.get('shopInUser')
+      ? JSON.parse(Cookies.get('shopInUser'))
+      : null
+  );
 
-  if (url.includes('/admin')) {
-    if (!userToken) {
-      return NextResponse.redirect('/login');
+  const [userToken, setUserToken] = useState(() =>
+    Cookies.get('userToken')
+      ? JSON.parse(Cookies.get('userToken'))
+      : null
+  );
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Login
+  const login = async (phone, password) => {
+    setIsLoading(true);
+    console.log(phone, password);
+    const response = await fetch(
+      'https://e-commerce-admin-fv58.onrender.com/api/auth/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ phone: phone, password: password }),
+      }
+    );
+    const userData = await response.json();
+    console.log('Inside Login', userData);
+    setIsLoading(false);
+    if (response.status === 200) {
+      setUser(userData);
+      setTempUser(userData);
+      setUserToken(userData.token);
+      Cookies.set('shopInUser', JSON.stringify(userData));
+      Cookies.set('userToken', JSON.stringify(userData.token));
+      router.push(userData.isAdmin ? '/admin' : '/');
+    } else {
+      toast.error(userData.message);
     }
+  };
 
-    try {
-      verify(userToken, secret);
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect('/login');
+  // Sign Up
+
+  const SignUp = async (data, e) => {
+    const createUser = {
+      phone: data.phone,
+      password: data.password,
+    };
+    console.log(createUser);
+
+    const response = await fetch(
+      'https://e-commerce-admin-fv58.onrender.com/api/auth/signup',
+      {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(createUser),
+      }
+    );
+    const userData = await response.json();
+    console.log(userData);
+    if (response.status === 201) {
+      setUser(userData.token);
+      setUserToken(userData.token);
+      Cookies.set('userToken', JSON.stringify(userData.token));
+      router.push('/');
+    } else {
+      toast.error(userData.message);
     }
-  }
+    e.target.reset();
+  };
 
-  return NextResponse.next();
+  // Logout user function
+  const Logout = async () => {
+    setUser(null);
+    setUserToken(null);
+    Cookies.remove('shopInUser');
+    Cookies.remove('userToken');
+    router.push('/');
+  };
+
+  const contextValue = {
+    user,
+    tempUser,
+    userToken,
+    isLoading,
+    login,
+    SignUp,
+    Logout,
+  };
+
+  return (
+    <MyContext.Provider value={contextValue}>
+      {props.children}
+    </MyContext.Provider>
+  );
 }
+
+export { MyContext, MyContextProvider };
